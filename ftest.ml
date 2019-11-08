b3023b2c575b5332bac127f244cd96eb6a43b98e
usernamedjpris <jeremie.gantet@gmail.com>
2019-11-08 11:01:54 +0100

    ftest

--- ftest.ml
+++ ftest.ml
@@ -1,5 +1,5 @@
 open Gfile
-    
+
 let () =
 
   (* Check the number of command-line arguments *)
@@ -11,20 +11,22 @@ let () =
 
 
   (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
-  
+
   let infile = Sys.argv.(1)
   and outfile = Sys.argv.(4)
-  
+
   (* These command-line arguments are not used for the moment. *)
-  and _source = int_of_string Sys.argv.(2)
-  and _sink = int_of_string Sys.argv.(3)
+  (* and _source = int_of_string Sys.argv.(2)
+     and _sink = int_of_string Sys.argv.(3)*)
   in
 
   (* Open file *)
   let graph = from_file infile in
 
+  let res = Tools.clone_nodes graph in 
+
   (* Rewrite the graph that has been read. *)
-  let () = write_file outfile graph in
+  let () = write_file outfile res in
 
   ()
 
--- tools.mli
+++ tools.mli
@@ -1,5 +1,6 @@
 (** module Tools *) 
-val gmap: 'a graph -> ('a -> 'b) -> 'b graph
+open Graph
+(*val gmap: 'a graph -> ('a -> 'b) -> 'b graph*)
 val clone_nodes: 'a graph -> 'b graph
-val add_arc: int graph -> id -> id -> int -> int graph
+val add_arcs: int graph -> id -> id -> int -> int graph
 
