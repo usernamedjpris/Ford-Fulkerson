2fee5649e05211ccdd9cf3461c312eca6f06de8b
RemiF <fache@etud.insa-toulouse.fr>
2019-11-14 11:02:40 +0100

    debut_find_path

---
+++ readme.txt
@@ -0,0 +1,4 @@
+compilation :
+ocamlbuild ftest.native
+(fichier ftest.ml et depnedencies compile en ftest.native)
+exec: ./ftest.native graphe_d1 a b resultat.txt
--- tools.ml
+++ tools.ml
@@ -1,4 +1,54 @@
-open Graph
+(*open Graph *)
+type id = int
+
+type 'a out_arcs = (id * 'a) list
+
+(* A graph is just a list of pairs: a node & its outgoing arcs. *)
+type 'a graph = (id * 'a out_arcs) list
+
+exception Graph_error of string
+
+let empty_graph = []
+
+let node_exists gr id = List.mem_assoc id gr
+
+let out_arcs gr id =
+  try List.assoc id gr
+  with Not_found -> raise (Graph_error ("Node " ^ string_of_int id ^ " does not exist in this graph."))
+
+let find_arc gr id1 id2 =
+  let out = out_arcs gr id1 in
+    try Some (List.assoc id2 out)
+    with Not_found -> None
+
+let new_node gr id =
+  if node_exists gr id then raise (Graph_error ("Node " ^ string_of_int id ^ " already exists in the graph."))
+  else (id, []) :: gr
+
+let new_arc gr id1 id2 lbl =
+
+  (* Existing out-arcs *)
+  let outa = out_arcs gr id1 in
+
+  (* Update out-arcs.
+   * remove_assoc does not fail if id2 is not bound.  *)
+  let outb = (id2, lbl) :: List.remove_assoc id2 outa in
+
+  (* Replace out-arcs in the graph. *)
+  let gr2 = List.remove_assoc id1 gr in
+    (id1, outb) :: gr2
+
+let n_iter gr f = List.iter (fun (id, _) -> f id) gr
+
+let n_iter_sorted gr f = n_iter (List.sort compare gr) f
+
+let n_fold gr f acu = List.fold_left (fun acu (id, _) -> f acu id) acu gr
+
+let e_iter gr f = List.iter (fun (id1, out) -> List.iter (fun (id2, x) -> f id1 id2 x) out) gr
+
+let e_fold gr f acu = List.fold_left (fun acu (id1, out) -> List.fold_left (fun acu (id2, x) -> f acu id1 id2 x) acu out) acu gr
+
+    
 
 
 (* rq: l graph à la sortie du ficher est un string, le mod avec gmap int_fo_string pour pouvoir considérer les labels comme des nombres et non des strings *) 
@@ -22,7 +72,26 @@ let gmap g f = e_fold g (fun gr id1 id2 lab-> new_arc gr id1 id2 (f lab)) (clone
 (*
 let res= gmap empty_graph int_of_string;;
 *)
+type labels =
+    { max: int;
+      current: int }
+let label_of_string s=
+  {max=(int_of_string s);current=0}
+let string_of_label l=
+  string_of_int(l.current)
 
-
+(* find arcs to use then add_arcs_labels min accu labels_arcs *)
+(* List.reverse à la fin *)
+let rec find_path gr id idfin accu =
+  if id = idfin then accu else
+    try let out=out_arcs id gr in 
+      let rec loop reste =
+        match reste with 
+          |[]->[]
+          |(id2,(m,c))::r-> if (m -c >0) then find_path gr id2 idfin ((id2,(m,c))::accu) else loop r   (* sinon mettre 2 ids si matter *)
+      in
+        loop out
+    with Not_found ->[]
+                       [];;
 
 
